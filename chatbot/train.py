73a22fe412804ba74b9bf176f8cfa96f67d2ba5d
felicityy-li <88757077+felicityy-li@users.noreply.github.com>
2022-07-31 23:01:15 -0400

    chore: create chatbot

---
+++ chatbot/train.py
@@ -0,0 +1,10 @@
+
+from chatbot import chatbot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+trainer.train(
+    "chatterbot.corpus.english"
+)
+
